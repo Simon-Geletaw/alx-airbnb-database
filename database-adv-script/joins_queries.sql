60a510de13f6b43ea4592316847ec70b47161a46
simon geletaw <simongeletaw2@gmail.com>
2025-10-30 10:27:54 -0400

    write complex query in sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,28 +10,5 @@ inner join
 airbnb_schema."Booking" as b on
 u.user_id=b.user_id;]
 
-[select p.name,
-p.description,p.location,
-COALESCE(r.rating,0),
-COALESCE(r.comment,'None')
-from airbnb_schema."Property" as p
-left join  airbnb_schema."Review" as r
-on p.property_id=r.property_id;]
-
-
-[select 
-u.first_name,
-u.last_name,
-u.email,
-COALESCE(TO_CHAR(b.start_date,'DD-Mon-YYYY')
-, 'none'),
-COALESCE(TO_CHAR(b.total_price,'9999999999')
-, 'none')
-from airbnb_schema."User" as u 
-full outer join 
-airbnb_schema."Booking" as b on
-u.user_id=b.user_id;
-]
-
 
 
